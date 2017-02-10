750f9586e7d10e84435a11ef21f6377dc0ba6694
Sasha <skmets@gmail.com>
2017-02-10 18:16:31 +0200

    master

--- src/main/scala/com/learning/scala_slides2/Source7.scala
+++ src/main/scala/com/learning/scala_slides2/Source7.scala
@@ -38,9 +38,57 @@ object Source7 {
     println()
     /* Вызываем функцию для всех индексов: */
     (0 to iArgs.length-1).foreach(i => print(iArgs(i) + " "))
+
+
+    println()
+    println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+    println( sum(fX,2,5) )
+    println( sumInts(2,5) ) /* #1 */
+    println( sumSquares(2,5) ) /* #2 */
+    println( sumPowersOfTwo(2,5) ) /* #3 */
   }
 
-  @tailrec def printArr(arr: Array[Int], pos: Int) {
+  /**
+   * Хвостовая рекурсия
+   * ******************
+   * @see http://ccfit.nsu.ru/~den/Scala/scala_slides2.pdf
+   */
+  def printArr(arr: Array[Int], pos: Int) { //@tailrec def printArr(arr: Array[Int], pos: Int) {
     if (pos<arr.length) { print(arr(pos) + " "); printArr(arr, pos+1) }
   }
+
+
+  /**
+   * Функции первого класса (функции высшего порядка)
+   * ************************************************
+   * @see https://ru.wikibooks.org/wiki/Scala_в_примерах
+   *
+   * (Функции в Scala являются "значениями первого класса")
+   * Функции которые принимают другие функции в качестве параметров или возвращают их как результаты, называются - "функциями высшего порядка"
+   */
+  def fX(a: Int) = a
+  /* Тип Int => Int — это тип функции, которая принимает аргумент типа Int и возвращает результат типа Int */
+  /* sum — функция высшего порядка, берущая в качестве параметра другую функцию */
+  def sum(f: Int => Int, a: Int, b: Int): Int =
+    if (a > b) 0 else f(a) + sum(f, a + 1, b)
+
+  /* #1 Написать функцию для суммирования всех целых чисел между двумя данными числами a и b */
+  def sumInts(a: Int, b: Int): Int =
+    if (a > b) 0 else a + sumInts(a + 1, b)
+
+  /* #2 Написать функцию для суммирования квадратов всех целых чисел между двумя данными числами a и b */
+  def square(x: Int): Int = x * x
+  def sumSquares(a: Int, b: Int): Int =
+    if (a > b) 0 else square(a) + sumSquares(a + 1, b)
+
+  /* #3. Написать функцию для суммирования степеней двойки 2n для всех целых чисел между двумя данными числами a и b */
+  def powerOfTwo(x: Int): Int = if (x == 0) 1 else 2 * powerOfTwo(x - 1)
+  def sumPowersOfTwo(a: Int, b: Int): Int =
+    if (a > b) 0 else powerOfTwo(a) + sumPowersOfTwo(a + 1, b)
+
+  /**
+   * "Каррирование" - Функций, возвращающие функции: эквивалентно предыдущему, но оно короче
+   * ************************************************
+   * @see https://ru.wikibooks.org/wiki/Scala_в_примерах
+   */
 }
